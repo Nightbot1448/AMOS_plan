9ca24682e0d94931aa687229cf51d647a4ae4677
Nightbot1448 <night1337bot@gmail.com>
2020-11-03 22:47:40 +0300

    all tests without removing non sign parametres

--- app/model_generator/generator.py
+++ app/model_generator/generator.py
@@ -4,11 +4,13 @@ from statistics import *
 
 class FullFactorModel:
     def __init__(self, count_of_parallel_experiments, count_of_factors):
-        # b0 + b1 * x1 + b2 * x2 + b12 * x1 * x2 
+        # b0 + b1 * x1 + b2 * x2 + b12 * x1 * x2 ##1 + cos(x1*x2)
         # TODO: chagne model to non parabaloid
         self.count_of_factors = count_of_factors
         self.count_of_points = 2**count_of_factors
         # points = np.ones((2**count_of_factors, count_of_factors))
+        
+        # TODO: центрирование и нормирование 
         # TODO: change to input
         points = np.array([
             [ 1,  1],
@@ -19,7 +21,9 @@ class FullFactorModel:
 
         self.count_of_parallel_experiments = count_of_parallel_experiments
         # TODO: generate model coefs
-        self.b_coef = np.array([ 2.245, 8.585, 39.945, 180.077])
+        # self.b_coef = np.array([ 2.245, 8.585, 39.945, 180.077])
+        self.b_coef = np.array([ 2.245, 0.3, 39.945, 180.077])
+
         
         self.coef = np.hstack((np.array([1]*4)[:, np.newaxis], points, (points[:,0] * points[:,1])[:, np.newaxis]))
         self.y_mean = (self.coef@self.b_coef)[:,np.newaxis]
@@ -31,10 +35,12 @@ class FullFactorModel:
             Дополнительный эксперимент. 
             Проводится по центральной точке
         """
-        self.y_mean = np.vstack((self.y_mean,[3]))
+
+        # --------------------------------------------------
+        self.y_mean = np.vstack((self.y_mean,[self.b_coef[0]]))
+        # --------------------------------------------------
         y_vals = np.array([self.y_mean[-1] for _ in range(self.count_of_parallel_experiments)]).reshape(1,-1)
         y_vals += np.random.normal(0,1, y_vals.shape)
-        print(self.y_vals.shape, y_vals.shape)
         self.y_vals = np.vstack((self.y_vals, y_vals))
 
         print(self.y_vals.round(3))
@@ -61,7 +67,7 @@ class FullFactorModel:
         prac_cochrain = self.cochran_value(var)
         prac_cochrain_crit = None
         numerator_val_cochran = self.count_of_parallel_experiments - 1
-        denominator_val_cochran = self.y_mean.size
+        denominator_val_cochran = self.count_of_points
         K_cochrain_index, N_cochrain_index = None, None
         try:
             K_cochrain_index = cochrain_K.index(numerator_val_cochran)
@@ -79,24 +85,28 @@ class FullFactorModel:
                 print('Не воспроизводится')
             
             print('Дисперсия ошибки (воспроизводимости эксперимента):', round(self.reproducibility_var,4))
+    
+    def get_estimate_parameters(self):
+        mean, var = self.points_mean_var()
+        self.prac_model_coef = self.coef @ mean / self.count_of_points
+        
+        self.var_params_of_model = self.reproducibility_var \
+            / (self.count_of_points * self.count_of_parallel_experiments)
+        print('Дисперсия параметра модели:', self.var_params_of_model)
+        return self.var_params_of_model
+
+    # def
 
     def significance_estimate_parameters(self):
         """
             Значимость оценок параметров. Критерий Стьюдента
-        """        
-        mean, var = self.points_mean_var()
-        prac_model_coef = self.coef @ mean / self.count_of_points
-        
-        var_params_of_model = self.reproducibility_var \
-            / (self.count_of_points * self.count_of_parallel_experiments)
-        print(round(var_params_of_model,4))
-
+        """
         # Наблюдаемое значение критерия Стьюдента =
         # асболютное значение коэф. / корень из дисперсии параметра модели var_params_of_model
-        prac_student_test = np.abs(prac_model_coef)/np.sqrt(var_params_of_model)
+        prac_student_test = np.abs(self.prac_model_coef)/np.sqrt(self.var_params_of_model)
         print(prac_student_test)
 
-        student_df = self.count_of_points * (count_of_parallel_experiments - 1)
+        student_df = self.count_of_points * (self.count_of_parallel_experiments - 1)
         crit_t_value_idx = None
         try:
             crit_t_value_idx = t_df.index(student_df)
@@ -105,30 +115,39 @@ class FullFactorModel:
         if crit_t_value_idx is not None:
             # > критерия == значимые
             crit_t_value = t_values_01[crit_t_value_idx]
-            print('Проверка значимости оценок параметров критерием Стьдента')
-            print(np.round(prac_model_coef, 4))
+            critstr = \
+                'Проверка значимости оценок параметров критерием Стьдента, критическое значение:'
+            print(critstr, round(crit_t_value,3))
+            print(np.round(self.prac_model_coef, 4))
             print(np.round(prac_student_test, 4))
-            is_significant = prac_student_test > crit_t_value
-            self.count_of_significant_coef = is_significant.sum()
-            print(is_significant)
-            if np.logical_not(is_significant).any():
-                print('Есть незначимый коэффициент')
+            self.significant_coef = prac_student_test > crit_t_value
+            print('Значимость:', self.significant_coef)
+            # self.count_of_significant_coef = is_significant.sum()
+            if np.logical_not(self.significant_coef).any():
+                print('Есть незначимый коэффициент. Надо его удалить')
+                print(self.prac_model_coef[self.significant_coef])
+                print(self.coef[:,self.significant_coef])
             else:
                 print('Все коэффициенты значимы')
+                
 
             #TODO: удаление и перерасчет при необходимости
     def adequacy(self):
         """
             Адекватность. Критерий Фишера.
         """
-        df_adequacy = self.count_of_points - self.count_of_significant_coef
+        df_adequacy = self.count_of_points - self.significant_coef.sum()
         if df_adequacy == 0:
             print('Нужен дополнительный эксперимент')
             self.additional_experiment()
+            self.adequacy_var = self.count_of_parallel_experiments * (self.y_vals[-1].mean() - self.b_coef[0])**2
+            print('Дисперсия адекватности', round(self.adequacy_var, 3))
+            self.prac_fisher_test = self.adequacy_var/self.reproducibility_var
+            print('Наблюдаемое значение критерия Фишера', round(fisher_test_res,3))
             
-            # self.count_of_parallel_experiments = 
-
+            # Если наблюдаемое значение больше критического, то модель неадекватна 
         # else:
+
 
 
 
--- app/model_generator/statistics.py
+++ app/model_generator/statistics.py
@@ -4,10 +4,12 @@ import scipy.stats
 cochrain_critical_values = np.genfromtxt('cochrain_values.csv', delimiter=',')
 cochrain_K = [2,3,4,5]
 cochrain_N = [2,3,4,5,6,7,8,9,10,12,15,20,24,30]
-t_df = [1,2,4,8,12,16,20,24,28,30,32,34,36,40,44,48,50,55,60,65,70,80,90,100,120,150]
+
 
 def t(alpha, gl):
     return scipy.stats.t.ppf(1-(alpha/2), gl)
 
+t_df = [1,2,4,8,12,16,20,24,28,30,32,34,36,40,44,48,50,55,60,65,70,80,90,100,120,150]
 t_values_01 = list(t(0.01,t_df))
 t_values_05 = list(t(0.05,t_df))
+
