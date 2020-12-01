26f6bddadc4099824b9f7459d19ec7d9374ec262
Dmitry Ivanov <darcenrall@gmail.com>
2020-12-01 21:30:24 +0300

    add task dict

--- flask/app/model_generator/generator.py
+++ flask/app/model_generator/generator.py
@@ -331,6 +331,17 @@ class FullFactorModel:
     def get_results(self):
         return self.__results
 
+
+MODEL_TASKS = {
+    1: FullFactorModel.variant_1,
+    2: FullFactorModel.variant_2,
+    3: FullFactorModel.variant_3,
+    4: FullFactorModel.variant_4,
+    5: FullFactorModel.variant_5,
+    6: FullFactorModel.variant_6,
+}
+
+
 if __name__ == '__main__':
     np.set_printoptions(suppress=True)
     count_of_parallel_experiments = 3
