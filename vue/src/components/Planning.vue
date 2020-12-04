818773f6b2593a073949f649736e795546b1b9ea
justaleaf <nik.vag@mail.ru>
2020-12-04 13:59:24 +0300

    Change planning page & add api for point number

--- vue/src/components/Planning.vue
+++ vue/src/components/Planning.vue
@@ -1,72 +1,42 @@
 <template>
   <div>
     <p>
-      <label for="number_of_points">Количество точек в спектре плана</label>
+      <label for="number_of_points">Сколько точек должно быть в спектре плана?</label>
       <input id="number_of_points" type="number" v-model="number_of_points" />
     </p>
-    <table class="table table-borderless table-responsive">
-      <thead>
-        <tr>
-          <th scope="col"></th>
-          <th scope="col">Значения Х1</th>
-          <th scope="col">Значения Х2</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th scope="row">Точка 1</th>
-          <td><input type="number" step="0.01" id="x1" v-model="points[0][0]" /></td>
-          <td><input type="number" step="0.01" id="y1" v-model="points[0][1]" /></td>
-        </tr>
-        <tr>
-          <th scope="row">Точка 2</th>
-          <td><input type="number" step="0.01" id="x2" v-model="points[1][0]" /></td>
-          <td><input type="number" step="0.01" id="y2" v-model="points[1][1]" /></td>
-        </tr>
-        <tr>
-          <th scope="row">Точка 3</th>
-          <td><input type="number" step="0.01" id="x3" v-model="points[2][0]" /></td>
-          <td><input type="number" step="0.01" id="y3" v-model="points[2][1]" /></td>
-        </tr>
-        <tr>
-          <th scope="row">Точка 4</th>
-          <td><input type="number" step="0.01" id="x4" v-model="points[3][0]" /></td>
-          <td><input type="number" step="0.01" id="y4" v-model="points[3][1]" /></td>
-        </tr>
-      </tbody>
-    </table>
-    <p>
-      <label for="number_of_experiments">Количество параллельных опытов (не более 5)</label>
-      <input id="number_of_experiments" type="number" name="number_of_experiments" v-model="number_of_experiments" />
-    </p>
-    <p>
-      <button @click="save_points">Сохранить</button>
-    </p>
+    <button @click="send_number_of_points">Проверить</button>
+    {{ answer }}
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   created() {},
   data() {
     return {
+      endpoint: "http://127.0.0.1:5000/api/check/plan_points_number",
+      answer: "",
       number_of_points: 4,
-      points: [
-        [1, 1],
-        [-1, 1],
-        [1, -1],
-        [-1, -1],
-      ],
-      number_of_experiments: 5,
-      randomization: false,
     };
   },
   props: {},
   methods: {
-    save_points: function (e) {
-      this.$store.dispatch("changePPoints", this.points);
-      this.$store.dispatch("changePPointsLen", this.number_of_points);
-      this.$store.dispatch("changeNExperiments", this.number_of_experiments);
+    send_number_of_points: function (e) {
+      axios
+        .post(this.endpoint, {
+          data: {
+            plan_points_number: this.number_of_points,
+          },
+        })
+        .then((response) => {
+          if (response.data.message === "") this.answer = "Правильно!";
+          else this.answer = response.data.message;
+        })
+        .catch((error) => {
+          console.log("-----error-------");
+          console.log(error);
+        });
     },
   },
 };
