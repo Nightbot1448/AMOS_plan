fd10dbf80fcdfb740b196bcc15f6dc44e2b80ec8
justaleaf <nik.vag@mail.ru>
2020-12-15 12:33:26 +0300

    Add poor documentation

--- vue/src/components/Preparation.vue
+++ vue/src/components/Preparation.vue
@@ -1,10 +1,29 @@
 <template>
   <div>
-    <ul>
-      <li><router-link class="nav-link" to="/task">Задание (выбор варианта)</router-link></li>
-      <li><router-link class="nav-link" to="/planning_area">Область планирования</router-link></li>
-      <li><router-link class="nav-link" to="/planning">Планирование</router-link></li>
-    </ul>
+    <div>
+      <ul>
+        <li>
+          <router-link class="nav-link" to="/task">Задание (выбор варианта)</router-link>
+        </li>
+        <li>
+          <router-link class="nav-link" to="/planning_area"
+            >Область планирования</router-link
+          >
+        </li>
+        <li><router-link class="nav-link" to="/planning">Планирование</router-link></li>
+      </ul>
+    </div>
+    <div class="documentation">
+      <p>Этап подготовки эксперимента предусматривает определение режима проведения работы, задание исходных данных, необходимых для построения плана и проведения эксперимента, а также построение плана эксперимента.</p>
+      <p>
+        Задание исходных данных включают в себя:
+        <ul>
+          <li>задание объекта исследования</li>
+          <li>задание области планирования</li>
+          <li>выбор типа плана эксперимента</li>
+        </ul>
+      </p>
+    </div>
   </div>
 </template>
 
--- vue/src/components/Task.vue
+++ vue/src/components/Task.vue
@@ -3,11 +3,24 @@
     <p>
       <label for="variant">Номер варианта задания</label>
       <select id="variant" v-model.number="variant" type="number">
-        <option v-for="(n, index) in 6" :key="index">{{ index + 1 }}</option>
+        <option v-for="(n, index) in 6" :key="index">
+          {{ index + 1 }}
+        </option>
       </select>
       <button @click="send_variant">Сохранить</button>
     </p>
     <router-link class="nav-link" to="/planning_area">Далее</router-link>
+    <div class="documentation">
+      <p>
+        В лабораторных работах объект, над которым проводится эксперимент, внешне
+        представляется как "черный ящик" с определенным числом независимых факторов и
+        одним откликом.
+      </p>
+      <p>
+        Конкретное число независимых факторов и функциональная связь между факторами и
+        откликом определяется вариантом задания.
+      </p>
+    </div>
   </div>
 </template>
 
