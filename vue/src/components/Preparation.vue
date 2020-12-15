6a99301a4c7286a8e6875b32614eaffd5828d05a
justaleaf <nik.vag@mail.ru>
2020-12-15 13:02:45 +0300

    Change preparation design

--- vue/src/components/Preparation.vue
+++ vue/src/components/Preparation.vue
@@ -1,19 +1,17 @@
 <template>
   <div>
     <div>
-      <ul>
-        <li>
-          <router-link class="nav-link" to="/task">Задание (выбор варианта)</router-link>
-        </li>
-        <li>
-          <router-link class="nav-link" to="/planning_area"
-            >Область планирования</router-link
-          >
-        </li>
-        <li><router-link class="nav-link" to="/planning">Планирование</router-link></li>
-      </ul>
+      <b-list-group>
+        <b-list-group-item to="/task">Задание (выбор варианта)
+        </b-list-group-item>
+        <b-list-group-item to="/planning_area"> Область планирования
+          
+        </b-list-group-item>
+        <b-list-group-item to="/planning">Планирование</b-list-group-item>
+      </b-list-group>
     </div>
     <div class="documentation">
+      <b-card bg-variant="info">
       <p>Этап подготовки эксперимента предусматривает определение режима проведения работы, задание исходных данных, необходимых для построения плана и проведения эксперимента, а также построение плана эксперимента.</p>
       <p>
         Задание исходных данных включают в себя:
@@ -23,6 +21,7 @@
           <li>выбор типа плана эксперимента</li>
         </ul>
       </p>
+      </b-card>
     </div>
   </div>
 </template>
